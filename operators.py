3f80958ddcbda6a716b743970068ffbe0b3f9cb8
Leorah-Bathula <leorahbathula@gmail.com>
2025-07-08 14:21:44 +0530

    operators program

---
+++ operators.py
@@ -0,0 +1,8 @@
+a = 50 
+b =  30 
+print('Addition of (a+b):' , a + b)
+print("Multiplication of (a * b): ", a * b)
+print("Subtraction of (a -b): " , a - b)
+print("Division of (a / b): ", a / b)
+print("Floor Division of (a // b): ", a//b)
+print("Exponential of (a ** b): ", a ** b)
