ab8a5d5efcbe39f17e8ecbe535fb61801fbb8258
Leorah-Bathula <leorahbathula@gmail.com>
2025-07-11 10:09:42 +0530

    strings

--- python-coursework/class_work/Class work/strings.py
+++ python-coursework/class_work/Class work/strings.py
@@ -108,3 +108,89 @@ print(x.rfind("p")) # 5 (Returns the last occurrence of the substring.)
 print(x.index("l")) # 3 (Like find(), but raises an error if not found.)
 print(x.rindex("p")) # 5 (Like rfind(), but raises an error if not found.)
 print(x.count("p")) # 3 (Counts how many times 'p' appears.)
+
+
+# 4.String testing methods
+y = "CloudySkies"
+print(y.startswith("Clo")) #startswith(sub) Checks if the string starts with sub.
+print(y.endswith("ies")) #endswith(sub) Checks if the string ends with sub.
+print(y.isalpha()) #isalpha() Returns True if all characters are alphabets.
+print(y.isalnum()) # isalnum() Returns True if all characters are alphanumeric.
+# An alphanumeric character is a character that can be a letter (A-Z, a-z) or a number (0-9). It combines alphabetic and numeric characters. 
+print(y.islower()) # islower()  Returns True if all characters are lowercase.
+print(y.isupper()) # isupper() Returns True if all characters are uppercase.
+print(y.isspace()) # isspace() Returns True if all characters are whitespace.
+print(y.istitle()) # istitle() Returns True if the string is in the title case.
+print(y.isidentifier()) # isidentifier() Checks if the string is a valid Python identifier.
+print(y.isdecimal())  # Most strict; only base-10 digits ('0'–'9' and equivalents)
+print(y.isdigit()) # Allows additional digit characters like superscripts
+print(y.isnumeric()) # Checks if the string is a valid Python identifier.
+
+# 5. Replace and Modify Methods 
+print(y.replace("Cloudy","Cotton")) #replace(old,new) Replaces occurrences of old with new.
+
+# maketrans() Creates a translation table for translate().
+# translate(table) Replaces characters using a translation table.
+# create a transition table 
+table = str.maketrans("aeio","@310")
+
+# Apply translation to string 
+text = "Hello , my name is Jessie"
+translated = text.translate(table)
+print(translated)  # H3ll0 , my n@m3 1s J3ss13
+
+# 6. Splitting and joining methods
+z = "I,Love,Cookies"
+print(z.split(","))  # ['I', 'Love', 'Cookies'] Splits the string into a list by sep.
+# split(sep, 1)	Left to right	1 split
+print(z.rsplit(",",1)) # ['I,Love', 'Cookies']
+# rsplit(sep, 1)	Right to left	1 split Splits from the right side.
+
+k = "New\nYork\nCity"
+print(k.splitlines()) # Splits at line breaks (\n). ['New', 'York', 'City']
+
+# join() It joins elements of an iterable (like a list or tuple) into a single string, using the string you call it on as a separator.
+# Example 1: Joining words with space 
+words = ["I","Love","Chips"]
+sentence = " ".join(words)
+print(sentence)  # I Love Chips
+
+#  Example 2: Joining with hyphen
+items = ["apple" ,"banana","cherry"]
+result = "-".join(items)
+print(result) # apple-banana-cherry
+
+# Example 3: Joining with no separator (combine chars)
+chars = ['J', 'e', 's', 's', 'i', 'e']
+name = "".join(chars)
+print(name) # Jessie
+
+j = "apple-pieday"
+print(j.partition("-"))  # ('apple', '-', 'pieday') Splits into a 3-part tuple at first sep. (before, sep, after) from first match
+o = "pine-apple-day"
+print(o.rpartition("-")) # ('pine-apple', '-', 'day') Splits into a 3-part tuple at lastsep. 	(before, sep, after) from last match
+
+# 7. Whitespace & Trimming Methods
+
+s = "    Hello Jessie    "
+print(s.strip()) # Hello Jessie Removes leading and trailing characters (default: spaces).
+print(s.lstrip()) # Hello Jessie    Removes leading characters.
+print(s.rstrip()) #     Hello Jessie Removes trailing characters.
+
+# 8. Encoding & Decoding Methods
+#  encode() Encode a string to bytes
+text = "Hello Leorah"
+encoded = text.encode("utf-8")
+print(encoded) # b'Hello Leorah'
+
+# decode() Decode bytes back to string
+decoded = encoded.decode("utf-8")
+print(decoded) # Hello Leorah
+
+
+
+
+
+
+
+
