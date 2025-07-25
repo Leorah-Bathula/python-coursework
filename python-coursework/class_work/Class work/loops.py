786c1aa88901b1db699d9abf5fc5f94a54fce003
Leorah-Bathula <leorahbathula@gmail.com>
2025-07-25 15:00:04 +0530

    loops

--- python-coursework/class_work/Class work/loops.py
+++ python-coursework/class_work/Class work/loops.py
@@ -168,3 +168,291 @@ while ind <= length:
     
 else:
     print("Palindrome ")        
+
+
+for i in range(5):
+    print(i,end = " ") # 0 1 2 3 4
+
+for j in range(5):
+    for i in range(5):
+        print(i,end = " ")
+    print()   # outside loop rows , inside loops columns
+'''
+0 1 2 3 4 
+0 1 2 3 4 
+0 1 2 3 4 
+0 1 2 3 4 
+0 1 2 3 4 
+
+'''        
+for j in range(5):
+    for i in range(5):
+        print("*",end = " ")
+    print()    
+''' col = 0 1 2 3 4 
+# rows = 0 * * * * * 
+1 * * * * * 
+2 * * * * * 
+3 * * * * * 
+4 * * * * * 
+'''    
+
+#Patterns using nested loops 
+n = int(input("Enter the size: "))
+for row in range(n):
+    for col in range(n):
+        print("*",end = " ")
+    print()   
+'''
+Enter the size: 7
+* * * * * * * 
+* * * * * * * 
+* * * * * * * 
+* * * * * * * 
+* * * * * * * 
+* * * * * * * 
+* * * * * * *
+'''    
+
+# 
+n = int(input("Enter the size: "))
+for row in range(n):
+    for col in range(n):
+        print(row,end = " ")
+    print()
+
+'''
+Enter the size: 5
+0 0 0 0 0 
+1 1 1 1 1 
+2 2 2 2 2 
+3 3 3 3 3 
+4 4 4 4 4 
+'''        
+#
+n = int(input("Enter the size: "))
+for row in range(n):
+    for col in range(n):
+        print(col,end = " ")
+    print()    
+'''
+Enter the size: 7
+0 1 2 3 4 5 6 
+0 1 2 3 4 5 6 
+0 1 2 3 4 5 6 
+0 1 2 3 4 5 6 
+0 1 2 3 4 5 6 
+0 1 2 3 4 5 6 
+0 1 2 3 4 5 6 
+'''    
+
+#
+n = int(input("Enter the size: "))
+for row in range(n):
+    for col in range(row + 1):
+        print("*",end = " ")
+    print()  
+'''
+Enter the size: 7
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * 
+* * * * * * * 
+''' 
+
+#
+n = int(input("Enter the size: "))
+
+for row in range(n):
+    for col in range(n - row):
+        print("*",end = " ")
+    print()   
+
+'''
+Enter the size: 5
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+'''   
+#
+n = int(input("Enter the sizez: ")) 
+for row in range(n):
+    for spaces in range(n-row-1):
+        print(" ",end = " ")
+    for col in range(row + 1):
+        print("*",end = " ")
+    print()        
+
+'''
+Enter the size: 5
+        * 
+      * * 
+    * * * 
+  * * * * 
+* * * * * 
+'''  
+
+#
+n = int(input("Enter the size: "))
+
+for row in range(n):
+    for spaces in range(row):
+        print(" ",end = " ")
+    for col in range(n - row):
+        print("*",end = " ")
+    print() 
+
+'''
+Enter the size: 5
+* * * * * 
+  * * * * 
+    * * * 
+      * * 
+        * 
+
+'''     
+
+#
+n = int(input("Enter the size: "))
+
+for row in range(n):
+    if row <= n//2:
+        for col1 in range(row + 1):
+            print("*",end = " ")
+            
+    else:
+        for col2 in range(n-row):
+            print("*",end = " ")
+    print()   
+
+'''
+Enter the size: 7
+* 
+* * 
+* * * 
+* * * * 
+* * * 
+* * 
+* 
+'''
+
+# 
+n = int(input("Enter the size: "))
+
+for row in range(n):
+    for col in range(n):
+        if row == 0 or col == 0 or row == n-1 or col == n-1:
+            print("*",end = " ")
+            
+        else:
+            print(" ",end = " ")
+            
+    print()        
+
+'''
+Enter the size: 7
+* * * * * * * 
+*           * 
+*           * 
+*           * 
+*           * 
+*           * 
+* * * * * * * 
+'''    
+
+n = int(input("Enter the size: "))
+
+for row in range(n):
+    for col in range(n):
+        if row == 0 or col == 0 or row == n-1 or col == n-1 or row == n //2:
+            print("*",end = " ")
+            
+        else:
+            print(" ",end = " ")
+            
+    print()  
+
+'''
+Enter the size: 5
+* * * * * 
+*       * 
+* * * * * 
+*       * 
+* * * * * 
+
+'''
+
+n = int(input("Enter the size: "))
+
+for row in range(n):
+    for col in range(n):
+        if row == 0 or col == 0 or row == n-1 or col == n-1 or row == n //2 or col == n//2:
+            print("*",end = " ")
+            
+        else:
+            print(" ",end = " ")
+            
+    print() 
+'''
+Enter the size: 7
+* * * * * * * 
+*     *     * 
+*     *     * 
+* * * * * * * 
+*     *     * 
+*     *     * 
+* * * * * * * 
+'''    
+
+#
+n = int(input("Enter the size: "))
+
+for row in range(n):
+    for col in range(n):
+        if row == 0 or  row == n-1 or  col + row == n-1:
+            print("*",end = " ")
+            
+        else:
+            print(" ",end = " ")
+            
+    print()  
+'''
+# col + row == n - 1 is theb logic here 
+Enter the size: 7
+* * * * * * * 
+          *   
+        *     
+      *       
+    *         
+  *           
+* * * * * * * 
+'''    
+
+n = int(input("Enter the size: "))
+
+for row in range(n):
+    for col in range(n):
+        if col == row or  col + row == n-1:
+            print("*",end = " ")
+            
+        else:
+            print(" ",end = " ")
+            
+    print()  
+'''
+Enter the size: 8
+*             * 
+  *         *   
+    *     *     
+      * *       
+      * *       
+    *     *     
+  *         *   
+*             * 
+
+'''    
